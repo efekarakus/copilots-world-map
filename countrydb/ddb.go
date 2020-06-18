d5a198f0f617b64c4421f092fe9247e81e8bae31
Efe Karakus <karakuse@amazon.com>
2020-06-17 17:23:56 -0700

    fix: handle new item

--- countrydb/ddb.go
+++ countrydb/ddb.go
@@ -2,15 +2,19 @@ package countrydb
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 type ddbClient interface {
+	GetItem(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error)
+	PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error)
 	UpdateItem(input *dynamodb.UpdateItemInput) (*dynamodb.UpdateItemOutput, error)
 	ScanPages(input *dynamodb.ScanInput, fn func(*dynamodb.ScanOutput, bool) bool) error
 }
@@ -23,6 +27,7 @@ type ddb struct {
 func NewDynamoDB() DB {
 	sess := session.Must(session.NewSession())
 
+	log.Printf("Initializing DynamoDB client connected to table: %s\n", os.Getenv("TRAVELS_TABLE_NAME"))
 	// Create a DynamoDB client from just a session.
 	svc := dynamodb.New(sess)
 	return &ddb{
@@ -32,6 +37,14 @@ func NewDynamoDB() DB {
 }
 
 func (db *ddb) Save(country string) (int, error) {
+	isNew, err := db.isNewCountry(country)
+	if err != nil {
+		return 0, err
+	}
+	if isNew {
+		return db.putCountry(country)
+	}
+
 	out, err := db.client.UpdateItem(&dynamodb.UpdateItemInput{
 		TableName: aws.String(db.tableName),
 		Key: map[string]*dynamodb.AttributeValue{
@@ -91,4 +104,43 @@ func (db *ddb) UniqueTotal() (int, error) {
 		}
 	}
 	return unique, nil
+}
+
+func (db *ddb) isNewCountry(country string) (bool, error) {
+	resp, err := db.client.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(db.tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"Country": {
+				S:   aws.String(country),
+			},
+		},
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			return false, fmt.Errorf("countrydb: get item %s: (code: %s, msg: %s)", country, aerr.Code(), aerr.Message())
+		}
+		return false, fmt.Errorf("countrydb: get item %s: %w", country, err)
+	}
+	return len(resp.Item) == 0, nil
+}
+
+func (db *ddb) putCountry(country string) (int, error) {
+	_, err := db.client.PutItem(&dynamodb.PutItemInput{
+		TableName: aws.String(db.tableName),
+		Item: map[string]*dynamodb.AttributeValue{
+			"Country": {
+				S: aws.String(country),
+			},
+			"Visit": {
+				N: aws.String("1"),
+			},
+		},
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			return 0, fmt.Errorf("countrydb: put new item %s: (code: %s, msg: %s)", country, aerr.Code(), aerr.Message())
+		}
+		return 0, fmt.Errorf("countrydb: put new item %s: %v", country, err)
+	}
+	return 1, nil
 }
